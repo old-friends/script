3cb08c4aff73a80baebf4c8ee3f3a269f2b73584
old-friends <78013059+old-friends@users.noreply.github.com>
2021-01-26 16:14:02 +0530

    Create hbrg.sh

---
+++ hbrg.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# This is the simple bash script
+NAME=webserver-1
+PORT=8089
+IMAGE_NAME=oldfriends/web-server
+
+# Let's run a container and expose the ports accordingly
+docker run --name ${NAME} -it -d -p ${PORT}:80 -d --privileged=true ${IMAGE_NAME}:latest bash -c "/usr/sbin/init" > /dev/null
+
+# Let's enable some variable and then will check result on browser
+NODE_PORT=$(docker inspect webserver-1 | grep -i "HostPort" | head -1 | awk -F ':' '{print $2}' | sed 's/ "//g' | sed 's/"//g' )
+NODE_IP=$(ifconfig eth1 | grep -i "inet" | grep -v "inet6" | awk '{print $2}')
+
+# Echo the result which need to be apply on browser
+echo -e "#######################################"
+echo -e "Please run the below URL in any browser"
+echo -e "#######################################"
+echo -e "http://${NODE_IP}:${NODE_PORT}"
+echo -e "=-=-=-=-=-=-=-=-=-=-=-=-="
+echo -e "If this URL is not working then get the IP of your interface and run the below command"
+echo -e "http://<Interface IP>:${NODE_PORT}"
+echo -e "=-=-=-=-=-=-=-=-=-=-=-=-="
